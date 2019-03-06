2e3412533482656159feedb31e9c09e03f1c0ecf
Agarwal <Aman.Agarwal@philips.com>
2019-03-06 08:19:44 +0530

    api completed and docs updated

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ The REST API to the example app is described below.
 
 `POST /add_asset/`
 
-    curl -X POST https://udaantest.herokuapp.com/add_asset -H 'Content-Type: application/json'  -H 'content-type: multipart/form-data -F assetID=<string>
+    curl -X POST https://udaantest.herokuapp.com/add_asset -H 'Content-Type: application/json' \ -H 'content-type: multipart/form-data -F assetID=<string>
 
 ### Response
 
@@ -38,7 +38,7 @@ The REST API to the example app is described below.
 
 `POST /add_task/`
 
-    curl -X POST https://udaantest.herokuapp.com/add_task -H 'Content-Type: application/json'  -H 'content-type: multipart/form-data -F taskID=<string> -F frequency=<string>
+    curl -X POST https://udaantest.herokuapp.com/add_task -H 'Content-Type: application/json' \ -H 'content-type: multipart/form-data -F taskID=<string> -F frequency=<string>
 
 ### Response
 
@@ -57,7 +57,7 @@ The REST API to the example app is described below.
 
 `POST /add_worker/`
 
-    curl -X POST https://udaantest.herokuapp.com/add_worker -H 'Content-Type: application/json'  -H 'content-type: multipart/form-data -F workerID=<string>
+    curl -X POST https://udaantest.herokuapp.com/add_worker -H 'Content-Type: application/json' \ -H 'content-type: multipart/form-data -F workerID=<string>
 
 ### Response
 
@@ -105,3 +105,30 @@ The REST API to the example app is described below.
     Content-Length: 2
 
     {answer:[required Object]}
+
+## Allocate the tak to a worker
+
+### Request
+
+`GET /allocate_task
+
+    curl -X POST http://localhost:5000/allocate_task -H 'Content-Type: application/json' -F assetID=<String> -F workerID=<String> -F taskID=<String> -F timeOfAllocation=<TimeStamp> -F taskTobePerformedBy=<TimeStamp> 
+
+### Response
+
+    HTTP/1.1 200 OK
+    Date: Thu, 24 Feb 2011 12:36:30 GMT
+    Status: 200 OK
+    Connection: close
+    Content-Type: application/json
+    Content-Length: 2
+
+    "The task is successfuly alloted"
+
+_*All tiemstamps must be provided in the given format **%Y-%m-%d %H:%M:%S**.*_
+
+_*The information about the frequency was inadequate. This means I could only allot a worker to the a task but cannot check when the worker is free or not.*_
+
+_*I have used formdata to pass data to the API. So to evaluate the work, please use POST. It has a special input type where we an pass the parameters via formdata*_
+
+_*Database used is mySQL. The mySQL server is provided by remotemysql.com*_
--- app.py
+++ app.py
@@ -4,13 +4,24 @@ from flask import Flask, render_template, request, Response, jsonify
 import json
 import mysql.connector
 from flask_cors import CORS, cross_origin
+import datetime
 
 # Variables
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 cors = CORS(app)
-
+#functions 
+def verify(string,table):
+	mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+	mycursor = mydb.cursor()
+	columnName=table.lower()+"ID";
+	sql="SELECT * FROM "+table +" WHERE "+columnName+" = \""+string+"\";"
+	mycursor.execute(sql)
+	myresult = mycursor.fetchall()
+	if(len(myresult)==1):
+		return True
+	return False
 # Views
 @app.route('/', methods=["GET"])
 def home():
@@ -18,12 +29,13 @@ def home():
 @app.route('/add_asset', methods=["POST"])
 def add_asset():
 	try:
-		if(len(request.form.get("assetID"))>0):
+		assetID=request.form.get("assetID")
+		if(len(assetID)>0):
 			mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 			mycursor = mydb.cursor()
 			try:
-				sql = "INSERT INTO asset (assetID, workerID) VALUES (%s, %s)"
-				val = (request.form.get("assetID"), "")
+				sql = "INSERT INTO asset (assetID) VALUES (%s)"
+				val = (assetID)
 				mycursor.execute(sql, val)
 				mydb.commit()
 				return Response("The asset is sucessfuly added",200,mimetype="application/json")
@@ -37,12 +49,14 @@ def add_asset():
 @app.route('/add_task', methods=["POST"])
 def add_task():
 	try:
-		if(len(request.form.get("taskID"))>0 and len(request.form.get("frequency"))>0 ):
+		taskID=request.form.get("taskID")
+		frequency=request.form.get("frequency")
+		if(len(taskID)>0 and len(frequency)>0 ):
 			mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 			mycursor = mydb.cursor()
 			try:
-				sql = "INSERT INTO Task (taskID, frequency,assetID) VALUES (%s, %s,%s)"
-				val = (request.form.get("taskID"), request.form.get("frequency"), "")
+				sql = "INSERT INTO Task (taskID, frequency) VALUES (%s, %s)"
+				val = (taskID,frequency)
 				mycursor.execute(sql, val)
 				mydb.commit()
 				return Response("The task is sucessfuly added",200,mimetype="application/json")
@@ -56,12 +70,14 @@ def add_task():
 @app.route('/add_worker', methods=["POST"])
 def add_worker():
 	try:
-		if(len(request.form.get("workerID"))>0):
+		workerID=request.form.get("workerID")
+
+		if(len(workerID)>0):
 			mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 			mycursor = mydb.cursor()
 			try:
-				sql = "INSERT INTO worker (workerID, taskID,status) VALUES (%s, %s,%s)"
-				val = (request.form.get("workerID"), "", "available")
+				sql = "INSERT INTO worker (workerID, status,taskID,timeOfAllocation,taskTobePerformedBy) VALUES (%s, %s,%s,%s,%s)"
+				val = (workerID, "available","NULL",datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 				mycursor.execute(sql, val)
 				mydb.commit()
 				return Response("The worker is sucessfuly added",200,mimetype="application/json")
@@ -79,29 +95,79 @@ def all():
 		mycursor = mydb.cursor()
 		mycursor.execute("SELECT * FROM asset")
 		myresult = mycursor.fetchall()
-		return jsonify(answer=myresult)
+		final=[]
+		for x in myresult:
+			print(x)
+			final.append(x[0])
+		return jsonify(ListOfAllAssests=final)
 	except:
 		return Response("Incorrect way fo accessing the API. Choose the correct format. Make sure you pass through the formData or the Database in down for maintainence",400,mimetype="application/json")
 
-# @app.route('/allocate_task', methods=["POST"])
-# def allocate_task():
-#     return "aman"
-
 @app.route('/get_tasks_for_workers/<workerID>', methods=["GET"])
 def get_allocate_task_for_worker(workerID):
-	# try:
 	mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 	mycursor = mydb.cursor()
-	sql="SELECT * FROM worker" 
+	sql="SELECT * FROM worker WHERE workerID=\""+workerID+"\""; 
 	mycursor.execute(sql)
 	myresult = mycursor.fetchall()
-	for x in myresult:
-		if(x[0]==workerID):
-			return jsonify(answer=x)
+	final={}
+	if(len(myresult)==1):
+		final["workerID"]=myresult[0][0]
+		final["taskID"]=myresult[0][1]
+		final["status"]=myresult[0][2]
+		final["timeOfAllocation"]=myresult[0][3]
+		final["taskTobePerformedBy"]=myresult[0][4]
+		return jsonify(answer=final)
 	return Response("No such worker exists",status=400,mimetype="application/json")
-	
-	# except:
-	# 	return Response("Incorrect way fo accessing the API. Choose the correct format. Make sure you pass through the formData or the Database in down for maintainence",400,mimetype="application/json")
+
+@app.route('/allocate_task', methods=["POST"])
+def allocate_task():
+	workerID=request.form.get("workerID")
+	assetID=request.form.get("assetID")
+	taskID=request.form.get("taskID")
+	timeOfAllocation=request.form.get("timeOfAllocation")
+	taskTobePerformedBy=request.form.get("taskTobePerformedBy")
+	if(len(assetID)>0 and len(taskID)>0 and len(workerID)>0): 
+		if(verify(assetID,"asset")):
+			if(verify(taskID,"Task")):
+				if(verify(workerID,"worker")):
+					mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+					mycursor = mydb.cursor()
+					sql="SELECT * FROM worker WHERE workerID= \""+workerID+"\";"
+					print(sql)
+					mycursor.execute(sql)
+					myresult1 = mycursor.fetchall()
+					if(myresult1[0][2]=="available"):
+						mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+						mycursor = mydb.cursor()
+						sql="UPDATE `worker` SET `workerID`=\""+workerID+"\",`taskID`=\""+taskID+"\",`status`=\"occupied\",`timeOfAllocation`=\""+timeOfAllocation+"\",`taskTobePerformedBy`=\""+taskTobePerformedBy+"\" WHERE `workerID`=\""+workerID+"\"";
+						mycursor.execute(sql)
+						mydb.commit()
+						return "Successfuly alloted the task"
+					else:
+						ts1 = myresult1[0][4]
+						ts2= timeOfAllocation
+						f = '%Y-%m-%d %H:%M:%S'
+						stored=datetime.datetime.strptime(ts1, f)
+						new=datetime.datetime.strptime(ts2, f)
+						if(new>stored):
+							mydb1 = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+							mycursor1 = mydb1.cursor()
+							sql="UPDATE `worker` SET `workerID`=\""+workerID+"\",`taskID`=\""+taskID+"\",`status`=\"occupied\",`timeOfAllocation`=\""+timeOfAllocation+"\",`taskTobePerformedBy`=\""+taskTobePerformedBy+"\" WHERE `workerID`=\""+workerID+"\"";
+							mycursor1.execute(sql)
+							mydb1.commit()
+							return "Successfuly alloted the task"
+						else:	
+							return Response("The worker is Busy",status=400,mimetype="application/json")
+				else:
+					return Response("WorkerID not registered",status=400,mimetype="application/json")
+			else:
+				return Response("taskID not registered",status=400,mimetype="application/json")
+		else:
+			return Response("assetID not registered",status=400,mimetype="application/json")
+	else:
+		return Response("Missing parameters",status=400,mimetype="application/json")
+		
 
 # Run
 if __name__ == '__main__':
