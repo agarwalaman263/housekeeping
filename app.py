7d0246744245da9b64563f924f31bd1b1270f6cc
Agarwal <Aman.Agarwal@philips.com>
2019-03-06 08:51:29 +0530

    check for time

--- app.py
+++ app.py
@@ -34,9 +34,8 @@ def add_asset():
 			mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 			mycursor = mydb.cursor()
 			try:
-				sql = "INSERT INTO asset (assetID) VALUES (%s)"
-				val = (assetID)
-				mycursor.execute(sql, val)
+				sql = "INSERT INTO asset VALUES ('"+assetID+"');"
+				mycursor.execute(sql)
 				mydb.commit()
 				return Response("The asset is sucessfuly added",200,mimetype="application/json")
 			except:
@@ -55,7 +54,7 @@ def add_task():
 			mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 			mycursor = mydb.cursor()
 			try:
-				sql = "INSERT INTO Task (taskID, frequency) VALUES (%s, %s)"
+				sql = "INSERT INTO Task VALUES (%s, %s)"
 				val = (taskID,frequency)
 				mycursor.execute(sql, val)
 				mydb.commit()
@@ -122,52 +121,57 @@ def get_allocate_task_for_worker(workerID):
 
 @app.route('/allocate_task', methods=["POST"])
 def allocate_task():
-	workerID=request.form.get("workerID")
-	assetID=request.form.get("assetID")
-	taskID=request.form.get("taskID")
-	timeOfAllocation=request.form.get("timeOfAllocation")
-	taskTobePerformedBy=request.form.get("taskTobePerformedBy")
-	if(len(assetID)>0 and len(taskID)>0 and len(workerID)>0): 
-		if(verify(assetID,"asset")):
-			if(verify(taskID,"Task")):
-				if(verify(workerID,"worker")):
-					mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
-					mycursor = mydb.cursor()
-					sql="SELECT * FROM worker WHERE workerID= \""+workerID+"\";"
-					print(sql)
-					mycursor.execute(sql)
-					myresult1 = mycursor.fetchall()
-					if(myresult1[0][2]=="available"):
+	try:
+		workerID=request.form.get("workerID")
+		assetID=request.form.get("assetID")
+		taskID=request.form.get("taskID")
+		timeOfAllocation=request.form.get("timeOfAllocation")
+		taskTobePerformedBy=request.form.get("taskTobePerformedBy")
+		datetime.datetime.strptime(timeOfAllocation,'%Y-%m-%d %H:%M:%S')
+		datetime.datetime.strptime(taskTobePerformedBy,'%Y-%m-%d %H:%M:%S')
+
+		if(len(assetID)>0 and len(taskID)>0 and len(workerID)>0): 
+			if(verify(assetID,"asset")):
+				if(verify(taskID,"Task")):
+					if(verify(workerID,"worker")):
 						mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
 						mycursor = mydb.cursor()
-						sql="UPDATE `worker` SET `workerID`=\""+workerID+"\",`taskID`=\""+taskID+"\",`status`=\"occupied\",`timeOfAllocation`=\""+timeOfAllocation+"\",`taskTobePerformedBy`=\""+taskTobePerformedBy+"\" WHERE `workerID`=\""+workerID+"\"";
+						sql="SELECT * FROM worker WHERE workerID= \""+workerID+"\";"
+						print(sql)
 						mycursor.execute(sql)
-						mydb.commit()
-						return "Successfuly alloted the task"
-					else:
-						ts1 = myresult1[0][4]
-						ts2= timeOfAllocation
-						f = '%Y-%m-%d %H:%M:%S'
-						stored=datetime.datetime.strptime(ts1, f)
-						new=datetime.datetime.strptime(ts2, f)
-						if(new>stored):
-							mydb1 = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
-							mycursor1 = mydb1.cursor()
+						myresult1 = mycursor.fetchall()
+						if(myresult1[0][2]=="available"):
+							mydb = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+							mycursor = mydb.cursor()
 							sql="UPDATE `worker` SET `workerID`=\""+workerID+"\",`taskID`=\""+taskID+"\",`status`=\"occupied\",`timeOfAllocation`=\""+timeOfAllocation+"\",`taskTobePerformedBy`=\""+taskTobePerformedBy+"\" WHERE `workerID`=\""+workerID+"\"";
-							mycursor1.execute(sql)
-							mydb1.commit()
+							mycursor.execute(sql)
+							mydb.commit()
 							return "Successfuly alloted the task"
-						else:	
-							return Response("The worker is Busy",status=400,mimetype="application/json")
+						else:
+							ts1 = myresult1[0][4]
+							ts2= timeOfAllocation
+							f = '%Y-%m-%d %H:%M:%S'
+							stored=datetime.datetime.strptime(ts1, f)
+							new=datetime.datetime.strptime(ts2, f)
+							if(new>stored):
+								mydb1 = mysql.connector.connect(host="remotemysql.com", user="6TvJRryz6j", passwd="Th1UfQOONG",  database="6TvJRryz6j") 
+								mycursor1 = mydb1.cursor()
+								sql="UPDATE `worker` SET `workerID`=\""+workerID+"\",`taskID`=\""+taskID+"\",`status`=\"occupied\",`timeOfAllocation`=\""+timeOfAllocation+"\",`taskTobePerformedBy`=\""+taskTobePerformedBy+"\" WHERE `workerID`=\""+workerID+"\"";
+								mycursor1.execute(sql)
+								mydb1.commit()
+								return "Successfuly alloted the task"
+							else:	
+								return Response("The worker is Busy",status=400,mimetype="application/json")
+					else:
+						return Response("WorkerID not registered",status=400,mimetype="application/json")
 				else:
-					return Response("WorkerID not registered",status=400,mimetype="application/json")
+					return Response("taskID not registered",status=400,mimetype="application/json")
 			else:
-				return Response("taskID not registered",status=400,mimetype="application/json")
+				return Response("assetID not registered",status=400,mimetype="application/json")
 		else:
-			return Response("assetID not registered",status=400,mimetype="application/json")
-	else:
-		return Response("Missing parameters",status=400,mimetype="application/json")
-		
+			return Response("Missing parameters",status=400,mimetype="application/json")
+	except:
+		return Response("Incorrect way fo accessing the API. Choose the correct format. Make sure you pass through the formData or the Database in down for maintainence",400,mimetype="application/json")		
 
 # Run
 if __name__ == '__main__':
